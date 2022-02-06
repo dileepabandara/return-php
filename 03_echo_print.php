8ae9b17c2040b38644ae62de60f07dfdaf86844b
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-06 22:39:35 +0530

    Create 03_echo_print.php

---
+++ 03_echo_print.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+    $txt1 = "Dileepa Bandara";
+    $txt2 = "Student & Developer";
+    $txt3 = "Software Development";
+    $x = 10;
+    $y = 15;
+
+    print "<h2>" . $txt1 . "</h2>";
+    print "Hello World! I am a " . $txt2 . "<br>";
+    print $x + $y;
+    echo " is this year";
+    print " and<br>";
+    echo "I love $txt3!";
+    ?>
+
+</body>
+
+</html>
